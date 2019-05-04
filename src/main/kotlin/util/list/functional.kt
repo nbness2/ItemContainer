4360f59cf427548c8e3c1edfc9d073386b519a3e
nbness2 <nbness1337@gmail.com>
2019-05-04 02:50:52 -0600

    moved stuff out of my namesake folder

--- src/main/kotlin/util/list/functional.kt
+++ src/main/kotlin/util/list/functional.kt
@@ -1,9 +1,9 @@
-package nbness.util.array
+package util.list
 
 /**
  * Partitions the Array in to 2 parts decided by [predicate] and joins them together.
  */
-inline fun <T> Array<T>.partitionJoin(joinLeft: Boolean=true, predicate: (T) -> Boolean): List<T> {
+inline fun <T> List<T>.partitionJoin(joinLeft: Boolean=true, predicate: (T) -> Boolean): List<T> {
     val results = partition(predicate)
     return if (joinLeft) results.first + results.second
     else results.second + results.first
