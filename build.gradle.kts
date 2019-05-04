47148423b85c8e8ce933a42ac866572abd8455d3
nbness2 <nbness1337@gmail.com>
2019-05-04 02:36:20 -0600

    update gitignore, added gradle stuff

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.30"
+    id("org.jetbrains.dokka") version "0.9.17"
+}
+
+group = "nbness"
+version = "1.14"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs = listOf("-Xuse-experimental=kotlin.contracts.ExperimentalContracts")
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
+    outputFormat = "html"
+    outputDirectory = "documentation\\html"
+}
